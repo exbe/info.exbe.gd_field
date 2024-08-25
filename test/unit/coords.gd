40989ae60715e68f21b91a415ad783eb32d39395
Alex M <exbeinfo@gmail.com>
2024-08-25 13:25:16 -0700

    re-organize testing for coords

--- test/unit/coords.gd
+++ test/unit/coords.gd
@@ -1,17 +1,18 @@
 extends GutTest
+var params_spec = ParameterFactory.named_parameters(
+	['dots', 'size', 'dot', 'expectedDot','message'], 
+	[   # values
+		[[Vector2i(9,14),Vector2i(80,33)], Vector2i(1,4),Vector2(-0.41, -0.86),Vector2i(9,14),"Exact dot should return it"],
+		[[Vector2i(9,14),Vector2i(80,33)], Vector2i(1,4),Vector2(-0.31, -0.76),Vector2i(9,14),"Close dot should return dot"]
+	])
 
-func test_similar_point():
+
+func test_similar_point2(params = use_parameters(params_spec)):
 	# this test will pass because 1 does equal 1
-	var dots :  Array[Vector2i] = [Vector2i(9,14),Vector2i(80,33)]
-	var field = Field.builder().size(Vector2i(1,4)).set_points(dots).build()
-	var coords = field.get_dot_of(Vector2(-0.41, -0.86))
+	var dots :  Array[Vector2i] = []
+	dots.assign(params.dots)
+	var field = Field.builder().size(params.size).set_points(dots).build()
+	var coords = field.get_dot_of(params.dot)
 	assert_eq(coords.size(),1)
-	assert_eq(Vector2i(9,14),coords[0])
+	assert_eq(params.expectedDot,coords[0], params.message)
 	
-func test_some_point():
-	# this test will pass because 1 does equal 1
-	var dots :  Array[Vector2i] = [Vector2i(9,14),Vector2i(80,33)]
-	var field = Field.builder().size(Vector2i(1,4)).set_points(dots).build()
-	var coords = field.get_dot_of(Vector2(-0.31, -0.76))
-	assert_eq(coords.size(),1)
-	assert_eq(Vector2i(9,14),coords[0])	
